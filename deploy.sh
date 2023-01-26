cc93c20cf0aecf0be91437367e3676d9682dc020
Adrian Henle <eahenle@gmail.com>
2023-01-26 10:04:03 -0800

    .

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+julia --project -e 'using BigbrainCLI; deploy(ARGS)' $@
--- src/BigbrainCLI.jl
+++ src/BigbrainCLI.jl
@@ -1,14 +1,41 @@
 module BigbrainCLI
 
-using FIGlet
+using Comonicon
 import PrecompileSignatures: @precompile_signatures
 
-banner() = FIGlet.render("BigbrainCLI", FIGlet.availablefonts()[567])
+const BANNER = String(read(joinpath(dirname(pathof(BigbrainCLI)), "banner.txt")))
+banner() = println(BANNER)
 
 function __init__()
     banner()
 end
 
+function _deploy(appname::String)
+    PWD = pwd()
+    
+    try
+        cd(appname)
+    catch exception
+        @error "Could not deploy $appname" exception
+    end
+
+    try
+        run(Cmd(["julia", "--project", "deploy.jl"]))
+    catch exception
+        @error "Could not deploy $appname" exception
+    end
+
+    cd(PWD)
+end
+
+@main function main(apps)
+    _deploy.(apps)
+end
+
+deploy(apps::Vector{String}) = command_main(apps)
+
 @precompile_signatures BigbrainCLI
+
+export deploy
 
 end 
